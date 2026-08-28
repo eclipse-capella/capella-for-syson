a632cc0ced3b8df627dae48bb4676821c86bfeec
Thibault Béziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2026-08-28 11:04:58 +0200

    [42] Add diagram description
    Bug: https://github.com/eclipse-capella/capella-for-syson/issues/42
    Signed-off-by: Thibault Béziers la Fosse <thibault.beziers-la-fosse@obeo.fr>

---
+++ backend/views/capella-diagram-ocb-view/src/main/java/org/eclipse/capella/diagram/ocb/view/OCBViewConstants.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Obeo.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.capella.diagram.ocb.view;
+
+/**
+ * OCB diagram constants.
+ *
+ * @author tbezierslafosse
+ */
+public class OCBViewConstants {
+
+    public static final String COMPONENT_BACKGROUND_COLOR = "COMPONENT_BACKGROUND_COLOR";
+    public static final String COMPONENT_BORDER_COLOR = "COMPONENT_BORDER_COLOR";
+    public static final String COMPONENT_LABEL_COLOR = "COMPONENT_LABEL_COLOR";
+    public static final String DESCRIBES_BACKGROUND_COLOR = "DESCRIBES_BACKGROUND_COLOR";
+    public static final String COMPONENT_EXCHANGE_BACKGROUND_COLOR = "COMPONENT_EXCHANGE_BACKGROUND_COLOR";
+    public static final String REQUIREMENT_BACKGROUND_COLOR = "REQUIREMENT_BACKGROUND_COLOR";
+    public static final String REQUIREMENT_BORDER_COLOR = "REQUIREMENT_BORDER_COLOR";
+    public static final String REQUIREMENT_LABEL_COLOR = "REQUIREMENT_LABEL_COLOR";
+    public static final String CAPABILITY_BACKGROUND_COLOR = "CAPABILITY_BACKGROUND_COLOR";
+    public static final String CAPABILITY_BORDER_COLOR = "CAPABILITY_BORDER_COLOR";
+    public static final String CAPABILITY_LABEL_COLOR = "CAPABILITY_LABEL_COLOR";
+    public static final String INVOLVEMENT_COLOR = "INVOLVEMENT_COLOR";
+}
