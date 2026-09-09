617fcf1895827362673724159d9cae4a4fee6105
Thibault Béziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2026-09-09 17:28:48 +0200

    [108] Add OABD requirement node
    Bug: https://github.com/eclipse-capella/capella-for-syson/issues/108
    Signed-off-by: Thibault Béziers la Fosse <thibault.beziers-la-fosse@obeo.fr>

--- backend/views/capella-diagram-oabd-view/src/main/java/org/eclipse/capella/diagram/oabd/view/OABDViewDiagramDescriptionProvider.java
+++ backend/views/capella-diagram-oabd-view/src/main/java/org/eclipse/capella/diagram/oabd/view/OABDViewDiagramDescriptionProvider.java
@@ -15,6 +15,7 @@ package org.eclipse.capella.diagram.oabd.view;
 import java.util.List;
 
 import org.eclipse.capella.diagram.oabd.view.nodes.activity.OperationalActivityNodeDescriptionProvider;
+import org.eclipse.capella.diagram.oabd.view.nodes.requirement.RequirementNodeDescriptionProvider;
 import org.eclipse.capella.model.transverse.services.TransverseQueryService;
 import org.eclipse.sirius.components.view.builder.DefaultViewDiagramElementFinder;
 import org.eclipse.sirius.components.view.builder.providers.IDiagramElementDescriptionProvider;
@@ -55,7 +56,8 @@ public class OABDViewDiagramDescriptionProvider implements IRepresentationDescri
                 .build();
         var cache = new DefaultViewDiagramElementFinder();
         var diagramElementDescriptionProviders = List.of(
-                new OperationalActivityNodeDescriptionProvider(colorProvider)
+                new OperationalActivityNodeDescriptionProvider(colorProvider),
+                new RequirementNodeDescriptionProvider(colorProvider)
         );
 
         diagramElementDescriptionProviders.stream().map(IDiagramElementDescriptionProvider::create).forEach(cache::put);
