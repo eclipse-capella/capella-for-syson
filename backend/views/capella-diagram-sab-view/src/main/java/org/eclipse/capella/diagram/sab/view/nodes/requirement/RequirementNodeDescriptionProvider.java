35fbe523d42c8772b5b5d791d649d53a9550c8b9
Thibault Béziers la Fosse <thibault.beziers-la-fosse@obeo.fr>
2026-09-10 17:21:51 +0200

    [99] Add requirement compartment for LAB requirements
    Bug: https://github.com/eclipse-capella/capella-for-syson/issues/99

--- backend/views/capella-diagram-sab-view/src/main/java/org/eclipse/capella/diagram/sab/view/nodes/requirement/RequirementNodeDescriptionProvider.java
+++ backend/views/capella-diagram-sab-view/src/main/java/org/eclipse/capella/diagram/sab/view/nodes/requirement/RequirementNodeDescriptionProvider.java
@@ -21,6 +21,8 @@ import org.eclipse.sirius.components.view.diagram.NodeDescription;
 import org.eclipse.sirius.components.view.diagram.SynchronizationPolicy;
 import org.eclipse.sirius.components.view.diagram.UserResizableDirection;
 import org.eclipse.syson.sysml.SysmlPackage;
+import org.eclipse.syson.util.DescriptionNameGenerator;
+import org.eclipse.syson.util.IDescriptionNameGenerator;
 import org.eclipse.syson.util.ServiceMethod;
 import org.eclipse.syson.util.SysMLMetamodelHelper;
 
@@ -32,6 +34,8 @@ import org.eclipse.syson.util.SysMLMetamodelHelper;
 public class RequirementNodeDescriptionProvider extends AbstractNodeDescriptionProvider {
 
     public static final String NODE_DESCRIPTION_NAME = "RequirementNodeDescription";
+
+    private final IDescriptionNameGenerator nameGenerator = new DescriptionNameGenerator("SAB");
 
     public RequirementNodeDescriptionProvider(IColorProvider colorProvider) {
         super(colorProvider);
@@ -58,6 +62,10 @@ public class RequirementNodeDescriptionProvider extends AbstractNodeDescriptionP
         cache.getNodeDescription(NODE_DESCRIPTION_NAME).ifPresent(nodeDescription -> {
             diagramDescription.getNodeDescriptions().add(nodeDescription);
             nodeDescription.setPalette(new RequirementPaletteProvider(this.diagramBuilderHelper, this.viewBuilderHelper, this.nodeDeleteFromDiagramToolProvider).createNodePalette(nodeDescription, cache));
+            String documentationCompartmentName = this.nameGenerator.getCompartmentName(SysmlPackage.eINSTANCE.getRequirementUsage(),
+                    SysmlPackage.eINSTANCE.getElement_Documentation());
+            cache.getNodeDescription(documentationCompartmentName)
+                    .ifPresent(compartmentNode -> nodeDescription.getChildrenDescriptions().add(compartmentNode));
         });
     }
 }
